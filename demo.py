704b44c552ad7587a06e630c7f3c4dfd529d25d9
alvi_S2005 <alvishams2005@gmail.com>
2025-06-05 07:17:35 +0100

    Update demo.py
    new fresh start (we will revert to this eventuallt!)

--- demo.py
+++ demo.py
@@ -1,5 +1,1 @@
-print("hello nashy")
-print("hello nashy2")
-print("hello nashy3 ")
-print("hello nashy 4 (testing to see if i can push origin and get back when there was only 2 nashies)")
-print("i hate this bullshitbro pls")
+print("hello nashy NEWW")
