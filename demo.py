1aa4c6907405357af6a3e8692ef72add7384e0d1
alvi_S2005 <alvishams2005@gmail.com>
2025-06-05 06:50:52 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 print("hello nashy")
 print("hello nashy2")
 print("hello nashy3 ")
 print("hello nashy 4 (testing to see if i can push origin and get back when there was only 2 nashies)")
+=======
+print("hello nashy")
+>>>>>>> parent of dda0c46 (Update demo.py)
